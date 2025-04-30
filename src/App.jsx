208e0068ff3a58921400b30e8c24d3af4a905d48
Andrea Huang <huangandrea@hotmail.it>
2025-04-30 15:19:08 +0200

    State Scheduling & Batching

--- src/App.jsx
+++ src/App.jsx
@@ -10,11 +10,23 @@ function App() {
 
   const [chosenCount, setChosenCount] = useState(0);
 
+  function handleSetCount(newCount) {
+    // setChosenCount(newCount);
+    // setChosenCount(chosenCount + 1); // this will not work, will get the old
+    // value of chosenCount, that is in the case of first render 0
+
+    // state changes with the same function are batched, so the component
+    // is not rendered twice
+    setChosenCount(newCount);
+    setChosenCount(prevCount => prevCount + 1); // this will work, will get the new
+    // value of chosenCount
+  }
+
   return (
     <>
       <Header />
       <main>
-        <ConfigureCounter onSet={setChosenCount} />
+        <ConfigureCounter onSet={handleSetCount} />
         <Counter key={chosenCount} initialCount={chosenCount} />
       </main>
     </>
